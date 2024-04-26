2f1af53fcfe2b6d3524a99d296ab16da608d85e3
WilliamBaltazar <wbsbaltazar@gmail.com>
2024-04-25 22:11:02 -0300

    aula javascript

---
+++ aulajavascript2504/js/criandoobjetivo.js
@@ -0,0 +1,18 @@
+var obj = {
+    nome: "William",
+    idade: "29",
+    Profissão: "programador",
+    estatrabalhando: true,
+};
+ console.log(obj);
+ console.log(typeof obj)
+
+ //acessando um objeto
+
+ console.log(obj.nome);
+ console.log(obj.idade);
+ console.log(obj.Profissão);
+
+ //usando a propriedade objetivo
+ obj.nome = "Baltazar"
+ console.log(obj.nome);
---
+++ aulajavascript2504/js/vetor.js
@@ -0,0 +1,15 @@
+//criando um array[] se for() é um objeto
+var arr = [7,"thiago",true,{teste: 1,teste: 2}];
+
+console.log (arr);
+
+//array do mesmo tipo
+var arr2 = [1,2,3,4,5];
+
+// acessando elemento do array iniciando em 0
+console.log(arr[1]);
+
+//modificando elemento do array
+arr[1]="william";
+console.log(arr[1]);
+
